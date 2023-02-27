16aaa4e64eea7f1240149570494d4b196a0a2306
Linh Le <fenix.lin@yandex.ru>
2023-02-28 02:14:43 +0300

    Added cards images and shuffled covers

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+
+const cardsImages = [
+  { src: "/icons/001.png" },
+  { src: "/icons/002.png" },
+  { src: "/icons/003.png" },
+  { src: "/icons/004.png" },
+  { src: "/icons/005.png" },
+  { src: "/icons/006.png" },
+  { src: "/icons/007.png" },
+  { src: "/icons/008.png" },
+];
 
 function App() {
+
+  const [cards, setCards] = useState([]);
+  const [turns, setTurns] = useState(0);
+
+  const resetGame = () => {
+    const duplicatedCards = [...cardsImages, ...cardsImages];
+    const shuffledCards = duplicatedCards
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random() }));
+
+      setCards(shuffledCards);
+      setTurns(0);
+
+      console.log(cards, turns);
+  };
+
   return (
-    <div className='App'>
+    <div className="App">
       <header>memory</header>
-      <button>new game</button>
+      <button onClick={resetGame}>new game</button>
     </div>
   );
 }
